ca70637d375e218742f14ec05defba51619e6df9
Juana Gazcon <juanigazcon96@gmail.com>
2022-07-19 14:19:25 -0300

    Libreria

--- js/logic.js
+++ js/logic.js
@@ -149,31 +149,33 @@ const contenedorCarrito = document.getElementById("carrito-contenedor");
 const accionesCarrito = document.getElementById("acciones")
 const opcionesDePago = document.getElementById("opciones-pago");
 const newDiv = document.createElement("div");
+//const side= document.getElementById("displayCarrito");
+
+//side.addEventListener("click", actualizarCarrito());
 
 let botonesDisponibles = false;
 let compraConfirmada = false;
 
 document.addEventListener("DOMContentLoaded", ()=> {
     
-        let checkout = localStorage.getItem("compra");
+       let checkout = localStorage.getItem("compra");
         CARRITO = JSON.parse(localStorage.getItem("CARRITO"));
         actualizarCarrito();
-       if(CARRITO!=null){
-            agregarBotones();
-        }
-        if(CARRITO==null){
-            vaciarCarrito();
-        }
+       CARRITO!=null && agregarBotones();
+       CARRITO==null && vaciarCarrito();
+        
         
     }
     )
+
+
 
 //AGREGAR PRODUCTOS AL CARRITO
 console.log(PRODUCTO)
 PRODUCTO.forEach(btn => {
 btn.addEventListener("click", () => {
     console.log(btn.dataset.id)
-    let productoElegido = CATALOGO.find(item => item.id === parseInt(btn.dataset.id));
+    let productoElegido = CATALOGO.find(item => item?.id === parseInt(btn.dataset.id));
     console.log(productoElegido);
     
     if (CARRITO.length == 0 && (!botonesDisponibles)) {
@@ -182,10 +184,23 @@ btn.addEventListener("click", () => {
     
     agregarAlCarrito(parseInt(btn.dataset.id));
     
-    if (CARRITO.length == 0) {
-        removerBotones();
-    }
+    CARRITO.length == 0 && removerBotones();
+    
     console.log(CARRITO);
+
+    Toastify({
+                 text: 'Agregaste ' + productoElegido.nombre + ' al carrito',
+                 duration: 2500,
+                 gravity: "bottom",
+                 position: "right",
+                 style: {
+                     
+                     color: "#e1ceb2",
+                     background: "#794245"
+                 },
+                 stopOnFocus: true,
+                 //close: true,
+            }).showToast();
 });
 
 });
@@ -198,7 +213,7 @@ botonesDisponibles = true;
 const buttons = document.createElement("div", "lala");
 buttons.innerHTML = `
 <button onclick="vaciarCarrito()" class="boton" id="botonC">Anular compra</button>
-<button onclick="confirmarCarrito()" class="boton">Confirmar compra</button>
+<button onclick="confirmarCarrito()" class="boton" style:"none">Confirmar compra</button>
 `
 const botonC = document.getElementById("botonC")
 accionesCarrito.appendChild(buttons);
@@ -224,7 +239,6 @@ quantity.innerHTML = cantidadCarrito;
 
 //CALCULAR PRECIO TOTAL DEL CARRITO
 function calcularPrecio() {
-
 precioCarrito = CARRITO.reduce((acc, elemento) => acc + (elemento.precio * elemento.cantidad), 0);
 console.log(precioCarrito)
 let precioTotal = document.getElementById("total");
@@ -256,11 +270,12 @@ if (productoExistenteEnCarrito) {
 //ACTUALIZAR INFORMACIÓN EXISTENTE EN EL CARRITO
 function actualizarCarrito() {
 contenedorCarrito.innerHTML = " ";
+//side.innerHTML=" ";
 CARRITO.forEach(item => {
     const div = document.createElement("div");
     div.className = "tabla"
     div.innerHTML = `
-<tr">
+<tr>
 <td>${item.nombre} </td>
 <td>Cantidad: <span id=cantidad>${item.cantidad}</span></td>
 <td>Precio: $${item.precio * item.cantidad}</td>
@@ -270,6 +285,7 @@ CARRITO.forEach(item => {
 </tr>
 `
     contenedorCarrito.appendChild(div);
+   // side.appendChild(div);
 
     localStorage.setItem("CARRITO", JSON.stringify(CARRITO));
 })
@@ -277,20 +293,74 @@ calcularCantidad();
 calcularPrecio();
 }
 
+
+
 //ELIMINAR PRODUCTO DEL CARRITO
 function eliminarProducto(id) {
-
-let productoEliminado = CARRITO.find(productoEliminado => productoEliminado.id === id)
+    //let productoAEliminar = CATALOGO.find(item => item.id === id);
+    let productoEliminado = CARRITO.find(productoEliminado => productoEliminado.id === id)
+    if(productoEliminado.cantidad > 0){
+    swal.fire(
+        {
+        title: 'Estás a punto de eliminar del carrito todas las unidades de ' + productoEliminado.nombre + '.',
+        text: 'No te lo recomiendo...',
+        showCancelButton:true,
+        showConfirmButton:true,
+        confirmButtonColor: '#794245',
+        confirmButtonText:'Eliminar',
+        cancelButtonColor: '#794245',
+        cancelButtonText:'Me arrepentí',
+        background: "#E1CEB2",
+        color: "#b49a7f",
+    }).then( result => {
+        if (result.isConfirmed) {
+            Toastify({
+                        text: 'El producto fue eliminado, vos te lo perdés!',
+                        duration: 3000,
+                        gravity: 'bottom',
+                        position: 'right',
+                        style:{background: "#794245"}
+                     }).showToast();
+//            let productoEliminado = CARRITO.find(productoEliminado => productoEliminado.id === id)
 CARRITO.splice(CARRITO.indexOf(productoEliminado), 1);
+                    
 actualizarCarrito();
 calcularCantidad();
 calcularPrecio();
 console.log(CARRITO);
+                    
+        }
+    } )} 
+    else{
+    Toastify({
+        text: 'No hay unidades para eliminar',
+        duration: 3000,
+        gravity: 'bottom',
+        position: 'right',
+        style:{background: "grey"}
+     }).showToast();
 
+} 
+    
 }
 
 //AGREGAR UNA UNIDAD AL CARRITO
 function aumentarCantidad(id) {
+    let productoElegido = CATALOGO.find(productoElegido => productoElegido.id === id);
+
+    Toastify({
+        text: 'Buenísimo, una unidad más de ' + productoElegido.nombre,
+        duration: 2500,
+        gravity: "bottom",
+        position: "right",
+        style: {
+            
+            color: "#e1ceb2",
+            background: "#794245"
+        },
+        stopOnFocus: true,
+        //close: true,
+   }).showToast();
 let productoModificado = CARRITO.find(productoModificado => productoModificado.id === id);
 productoModificado.cantidad++;
 actualizarCarrito();
@@ -304,6 +374,19 @@ function disminuirCantidad(id) {
 let productoModificado = CARRITO.find(productoModificado => productoModificado.id === id);
 if (productoModificado.cantidad > 0) {
     productoModificado.cantidad--;
+    Toastify({
+        text: 'Oh, una unidad menos de ' + productoModificado.nombre,
+        duration: 2500,
+        gravity: "bottom",
+        position: "right",
+        style: {
+            
+            color: "#e1ceb2",
+            background: "#794245"
+        },
+        stopOnFocus: true,
+        //close: true,
+   }).showToast();
     actualizarCarrito();
     calcularPrecio();
     console.log(CARRITO);
@@ -342,12 +425,31 @@ if (CARRITO.length != 0) {
     
     
 }
-
 }
 
 //ELIMINAR TODOS LOS PRODUCTOS DEL CARRITO
 function vaciarCarrito() {
-CARRITO.splice(0, CARRITO.length);
+    if(cantidadCarrito!=0){
+    swal.fire({
+        title: '¿Seguro querés vaciar el carrito?',
+        showCancelButton:true,
+        showConfirmButton:true,
+        confirmButtonColor: '#794245',
+        confirmButtonText:'Sí, vaciar carrito',
+        cancelButtonColor: '#794245',
+        cancelButtonText:'No, me arrepentí',
+        background: "#E1CEB2",
+        color: "#b49a7f"
+    }).then( result => {
+        if (result.isConfirmed) {
+            Toastify({
+                        text: 'Eliminaste todos los productos del carrito.',
+                        duration: 3000,
+                        gravity: 'center',
+                        position: 'center',
+                        style:{background: "#794245"}
+                     }).showToast();
+            CARRITO.splice(0, CARRITO.length);
 console.log(CARRITO);
 actualizarCarrito();
 calcularPrecio();
@@ -355,23 +457,71 @@ actualizarCarrito();
 removerBotones();
 newDiv.innerHTML = "";
 localStorage.setItem("CARRITO", JSON.stringify(CARRITO));
+        } else {
+            Toastify({
+                text: 'Buena decisión',
+                duration: 3000,
+                gravity: "bottom",
+                position: "right",
+                style:{background: "#794245"}
+             }).showToast();
 
-}
+        }
+    } ) 
+    } else {
+        newDiv.innerHTML = `<h3>El carrito está vacío</h3>`
+        opcionesDePago.appendChild(newDiv);
+
+}}
 
 //CALCULAR PRECIO EFECTIVO
 function pagoEfectivo() {
-const ultimoPrecio = document.getElementById("precioFinal");
-ultimoPrecio.innerHTML = "El precio total abondando en efectivo es $" + precioCarrito * 0.90;
+
+    swal.fire(
+        {
+        title: 'El precio total abonando en efectivo es $' + precioCarrito * 0.90 + '\n\n Por favor enviar mail con detalle de compra a totuspasteleria@gmail.com',
+        showConfirmButton:true,
+        confirmButtonColor: '#794245',
+        confirmButtonText:'Ok',
+        background: "#E1CEB2",
+        color: "#b49a7f",
+    })
+//const ultimoPrecio = document.getElementById("precioFinal");
+//ultimoPrecio.innerHTML = "El precio total abondando en efectivo es $" + precioCarrito * 0.90;
 }
 
 //CALCULAR PRECIO TRANSFERENCIA
 function pagoTransferencia() {
-const ultimoPrecio = document.getElementById("precioFinal");
-ultimoPrecio.innerHTML = "El precio total abondando con transferencia es $" + precioCarrito * 0.95;
+    swal.fire(
+        {
+        title: 'El precio total abonando con transferencia es $' + precioCarrito * 0.90 + '\n\n Alias: TOTUSPASTELERIA \n\n Por favor enviar mail con comprobante de transferencia a totuspasteleria@gmail.com',
+        showConfirmButton:true,
+        confirmButtonColor: '#794245',
+        confirmButtonText:'Ok',
+        background: "#E1CEB2",
+        color: "#b49a7f",
+    })
+//const ultimoPrecio = document.getElementById("precioFinal");
+//ultimoPrecio.innerHTML = "El precio total abondando con transferencia es $" + precioCarrito * 0.95;
 }
 
 //CALCULAR PRECIO MERCADOPAGO
 function pagoMP() {
-const ultimoPrecio = document.getElementById("precioFinal");
-ultimoPrecio.innerHTML = "El precio total abondando con MercadoPago es $" + precioCarrito;
+   
+    Toastify({
+        text: 'El precio total abonando con MercadoPago es $' + precioCarrito + '. \n Hacé click para avanzar al pago.',
+        destination: 'https://link.mercadopago.com.ar/totuspasteleria',
+        duration: 10000,
+        gravity: 'center',
+        position: 'center',
+        style: {
+            
+            color: "#e1ceb2",
+            background: "#794245"
+        },
+        stopOnFocus: true,
+        //close: true,
+   }).showToast();
+/* const ultimoPrecio = document.getElementById("precioFinal");
+ultimoPrecio.innerHTML = "El precio total abondando con MercadoPago es $" + precioCarrito; */
 }
